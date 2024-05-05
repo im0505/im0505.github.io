03ef1328f17989ba67d07e13c8b363c40b0bac86
Youngho Park <im05@kakao.com>
2024-05-05 16:59:03 +0900

    카메라 전환

--- ocr/script.js
+++ ocr/script.js
@@ -14,3 +14,40 @@ if (navigator.mediaDevices.getUserMedia) {
 } else {
   console.log('브라우저가 카메라 접근을 지원하지 않습니다.');
 }
+
+navigator.mediaDevices
+  .enumerateDevices()
+  .then((devices) => {
+    const cameras = devices.filter((device) => device.kind === 'videoinput');
+  })
+  .catch((error) => console.error('Device enumeration failed:', error));
+function getCameraStream(deviceId) {
+  const constraints = {
+    video: { deviceId: deviceId ? { exact: deviceId } : undefined },
+  };
+
+  return navigator.mediaDevices.getUserMedia(constraints);
+}
+let currentDeviceId;
+
+function switchCamera() {
+  navigator.mediaDevices
+    .enumerateDevices()
+    .then((devices) => {
+      const cameras = devices.filter((device) => device.kind === 'videoinput');
+      const currentIndex = cameras.findIndex(
+        (camera) => camera.deviceId === currentDeviceId
+      );
+      const nextIndex = (currentIndex + 1) % cameras.length;
+      currentDeviceId = cameras[nextIndex].deviceId;
+
+      return getCameraStream(currentDeviceId);
+    })
+    .then((stream) => {
+      const videoElement = document.querySelector('video');
+      videoElement.srcObject = stream;
+    })
+    .catch((error) => console.error('Failed to switch camera:', error));
+}
+
+document.body.querySelector('#switch').addEventListener('click', switchCamera);
